c5d296726f9664e1e65299564b85c33a9627f011
Heather Roberts <heather.roberts@canstar.com.au>
2017-12-13 11:43:57 +1000

    Add validation to ensure attributes use double quotes

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as colors from 'colors';
 
+// todo: configure files
 const files = ['../ng-website/src/app/app.component.html'];
 const errors: string[] = [];
 
@@ -10,6 +11,7 @@ function lint() {
     fs.readFile(filePath, (error, data) => {
       const fileString = data.toString();
       validateIndentation(filePath, fileString);
+      validateAttrQuotes(filePath, fileString);
 
       if (++lintedCount === files.length) {
         onLintingComplete();
@@ -37,7 +39,7 @@ function validateIndentation(filePath: string, fileString: string) {
     if (startIdx !== -1) {
       const diff = Math.abs(prevStartIdx - startIdx);
       if (diff !== indentation && diff !== 0) {
-        errors.push(`${filePath}:${idx} File should use 2 space indentation`);
+        errors.push(`${filePath}:${idx + 1} File should use 2 space indentation`);
       }
     } else {
       startIdx = 0;
@@ -47,7 +49,26 @@ function validateIndentation(filePath: string, fileString: string) {
   });
 }
 
-// todo: check attributes use double quotes
+function validateAttrQuotes(filePath: string, fileString: string) {
+  // todo: configure which quotes
+  const quote = `"`;
+
+  const attrRegex = /(\S+)=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/g;
+
+  const lines = fileString.split('\n');
+  lines.forEach((line: string, idx: number) => {
+    const attrs = line.match(attrRegex);
+    if (attrs) {
+      attrs.forEach(attrMatch => {
+        const split = attrMatch.split('=');
+        const attrQuote = split[1].replace(/s/g, '').charAt(0);
+        if (attrQuote !== quote) {
+          errors.push(`${filePath}:${idx + 1} Attributes should use single quotes`);
+        }
+      });
+    }
+  });
+}
 
 // todo: check spaces around equals
 
