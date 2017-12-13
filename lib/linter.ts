1ea0c47506bd861e6e5ec91af92f3e70942dcdcd
Heather Roberts <heather.roberts@canstar.com.au>
2017-12-14 09:51:57 +1000

    Fix issue where it wasn't detecting when all files were linted correctly

--- lib/linter.ts
+++ lib/linter.ts
@@ -10,36 +10,42 @@ export class Linter {
     return new Promise((resolve, reject) => {
       const errors: string[] = [];
       const filePaths = config.files;
-      let lintedCount = 0;
       if (!Array.isArray(filePaths)) {
         reject('files must be an array of file paths to lint (glob patterns are allowed)');
       } else {
         if (filePaths.length === 0) {
           reject('No files to lint');
         } else {
+          let lintedCount = 0;
+          let targetCount = 0;
           filePaths.forEach(filePath => {
             glob(filePath, (error, files) => {
-              files.forEach(file => {
-                fs.readFile(file, (error, data) => {
-                  if (error) {
-                    reject(error.toString());
-                  } else {
-                    const fileString = data.toString();
-                    const lines = fileString.split('\n');
+              if (error) {
+                reject(error.toString());
+              } else {
+                targetCount += files.length;
+                files.forEach(file => {
+                  fs.readFile(file, (error, data) => {
+                    if (error) {
+                      reject(error.toString());
+                    } else {
+                      const fileString = data.toString();
+                      const lines = fileString.split('\n');
 
-                    if (config.indentation) {
-                      errors.push(...Indentation.validate(file, lines, config.indentation));
-                    }
-                    if (config.attributes) {
-                      errors.push(...Attributes.validate(file, lines, config.attributes));
-                    }
+                      if (config.indentation) {
+                        errors.push(...Indentation.validate(file, lines, config.indentation));
+                      }
+                      if (config.attributes) {
+                        errors.push(...Attributes.validate(file, lines, config.attributes));
+                      }
 
-                    if (++lintedCount === filePaths.length) {
-                      resolve(errors);
+                      if (++lintedCount === targetCount) {
+                        resolve(errors);
+                      }
                     }
-                  }
+                  });
                 });
-              });
+              }
             });
           });
         }
