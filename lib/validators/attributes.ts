eb10173c829317f58d9aa6807f510a988e787c0f
Heather Roberts <heather.roberts@canstar.com.au>
2017-12-14 10:25:07 +1000

    Fix issue where attribute linting was running on content inside script tags

--- lib/validators/attributes.ts
+++ lib/validators/attributes.ts
@@ -8,17 +8,27 @@ export class Attributes {
     const errors: string[] = [];
 
     const attrRegex = /(\S+)\s?=\s?["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)["']?/g;
+    let inScriptTag = false;
     lines.forEach((line: string, idx: number) => {
-      const attrMatches = line.match(attrRegex);
-      if (attrMatches) {
-        attrMatches.forEach(attrMatch => {
-          if (config.whitespace != null && !AttrWhitespace.validate(attrMatch, config.whitespace)) {
-            errors.push(`${filePath}:${idx + 1} Attributes should have ${config.whitespace} whitespace around '=' character`);
-          }
-          if (config.quotes && !AttrQuotes.validate(attrMatch, config.quotes)) {
-            errors.push(`${filePath}:${idx + 1} Attributes should use ${config.quotes} quotes`);
-          }
-        });
+      // don't want to check contents of script tags
+      if (inScriptTag) {
+        if (line.indexOf('</script') !== -1) {
+          inScriptTag = false;
+        }
+      } else if (line.indexOf('<script') !== -1) {
+        inScriptTag = true;
+      } else {
+        const attrMatches = line.match(attrRegex);
+        if (attrMatches) {
+          attrMatches.forEach(attrMatch => {
+            if (config.whitespace != null && !AttrWhitespace.validate(attrMatch, config.whitespace)) {
+              errors.push(`${filePath}:${idx + 1} Attributes should have ${config.whitespace} whitespace around '=' character`);
+            }
+            if (config.quotes && !AttrQuotes.validate(attrMatch, config.quotes)) {
+              errors.push(`${filePath}:${idx + 1} Attributes should use ${config.quotes} quotes`);
+            }
+          });
+        }
       }
     });
 
